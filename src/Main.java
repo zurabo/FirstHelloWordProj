d0b2680b666ee65cd40ce3c0981d1265fb5e46d1
Ciyvi1998 <kakauridze.nika@gtu.ge>
2021-10-22 18:54:28 +0400

    commit from Nika

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World! Second try...");
+        System.out.println("Hello. This is Nika's edition");
     }
 }
